e924659d5a7543d9b4b61193284b55202259db79
Jim <jim@advancebass.com>
2023-05-12 14:49:57 -0700

    update schemes

--- swagger.js
+++ swagger.js
@@ -1,23 +1,26 @@
-require('dotenv').config()
+require('dotenv').config();
 const swaggerAutogen = require('swagger-autogen')();
 
-let host = process.env.NODE_ENV === 'development' ? `localhost:${process.env.PORT}` : `developerjokes.herokuapp.com`;
+let host, schemes;
+
+if (process.env.NODE_ENV === 'development') {
+  host = `localhost:${process.env.PORT}`;
+  schemes: ['http'];
+} else {
+  ('developerjokes.herokuapp.com');
+  schemes: ['https'];
+}
 
 const doc = {
   info: {
     title: 'DevJokes API',
     description: 'A practice REST API with cringe-worthy jokes',
   },
-  host: host,
+  schemes,
+  host,
 };
 
 const outputFile = './swagger_output.json';
 const endpointsFiles = ['./server.js'];
 
-swaggerAutogen(outputFile, endpointsFiles, doc)
-  // .then(() => {
-  //   require('./server');
-  // })
-  // .catch((err) => {
-  //   console.error(err);
-  // });
+swaggerAutogen(outputFile, endpointsFiles, doc);
